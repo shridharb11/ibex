eb887d6cc66ecb24c42c2a7d2338e6f34711ff30
shridharb11 <shridharb1106@gmail.com>
2026-06-04 17:17:18 +0530

    changed the packaged files

--- rtl/ibex_core.f
+++ rtl/ibex_core.f
@@ -2,20 +2,19 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-ibex_pkg.sv
-ibex_alu.sv
-ibex_compressed_decoder.sv
-ibex_controller.sv
-ibex_counter.sv
-ibex_cs_registers.sv
-ibex_decoder.sv
-ibex_ex_block.sv
-ibex_id_stage.sv
-ibex_if_stage.sv
-ibex_load_store_unit.sv
-ibex_multdiv_slow.sv
-ibex_multdiv_fast.sv
-ibex_prefetch_buffer.sv
-ibex_fetch_fifo.sv
-ibex_register_file_ff.sv
-ibex_core.sv
+rtl/core/ibex_core/rtl/ibex_alu.sv
+rtl/core/ibex_core/rtl/ibex_compressed_decoder.sv
+rtl/core/ibex_core/rtl/ibex_controller.sv
+rtl/core/ibex_core/rtl/ibex_counter.sv
+rtl/core/ibex_core/rtl/ibex_cs_registers.sv
+rtl/core/ibex_core/rtl/ibex_decoder.sv
+rtl/core/ibex_core/rtl/ibex_ex_block.sv
+rtl/core/ibex_core/rtl/ibex_id_stage.sv
+rtl/core/ibex_core/rtl/ibex_if_stage.sv
+rtl/core/ibex_core/rtl/ibex_load_store_unit.sv
+rtl/core/ibex_core/rtl/ibex_multdiv_slow.sv
+rtl/core/ibex_core/rtl/ibex_multdiv_fast.sv
+rtl/core/ibex_core/rtl/ibex_prefetch_buffer.sv
+rtl/core/ibex_core/rtl/ibex_fetch_fifo.sv
+rtl/core/ibex_core/rtl/ibex_register_file_ff.sv
+rtl/core/ibex_core/rtl/ibex_core.sv
