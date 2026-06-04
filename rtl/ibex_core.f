148d6b09077a9d0d4d1e20a7205c1b941e20efb8
shridharb11 <shridharb1106@gmail.com>
2026-06-04 19:56:52 +0530

    added pirm_clock_gating

--- rtl/ibex_core.f
+++ rtl/ibex_core.f
@@ -6,11 +6,13 @@ rtl/core/ibex_core/rtl/ibex_alu.sv
 rtl/core/ibex_core/rtl/ibex_compressed_decoder.sv
 rtl/core/ibex_core/rtl/ibex_controller.sv
 rtl/core/ibex_core/rtl/ibex_counter.sv
+rtl/core/ibex_core/rtl/ibex_csr.sv
 rtl/core/ibex_core/rtl/ibex_cs_registers.sv
 rtl/core/ibex_core/rtl/ibex_decoder.sv
 rtl/core/ibex_core/rtl/ibex_ex_block.sv
 rtl/core/ibex_core/rtl/ibex_id_stage.sv
 rtl/core/ibex_core/rtl/ibex_if_stage.sv
+rtl/core/ibex_core/rtl/ibex_wb_stage.sv
 rtl/core/ibex_core/rtl/ibex_load_store_unit.sv
 rtl/core/ibex_core/rtl/ibex_multdiv_slow.sv
 rtl/core/ibex_core/rtl/ibex_multdiv_fast.sv
